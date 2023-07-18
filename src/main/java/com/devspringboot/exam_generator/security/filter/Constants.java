4d10d7ecb0c3c94542a9e27c6d5b2de73247442c
Julio C. Mesquita <juliocesarmcamilo@gmail.com>
2023-07-17 23:44:38 -0300

    Constants_AutheticationFilter

---
+++ src/main/java/com/devspringboot/exam_generator/security/filter/Constants.java
@@ -0,0 +1,16 @@
+package com.devspringboot.exam_generator.security.filter;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.concurrent.TimeUnit;
+
+public class Constants {
+    public static final String SECRET = "secret";
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 86400000L; //1 DAY
+    public static void main(String... args){
+        System.out.println(TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
+        System.out.println(new BCryptPasswordEncoder().encode("julio"));
+    }
+}
